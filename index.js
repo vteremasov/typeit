aed21bd6e2294e4cb413ae149794999a212a98a6
Vitaly Teremasov <vitalii.t@miro.com>
2022-03-29 03:27:00 +0600

    Add line numbers Add scale for sharp text

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const canvas        = document.getElementById('canvas');
 const renderButton  = document.getElementById('render');
 const progress      = document.querySelector('.progress');
 const textArea      = document.getElementById('text');
-const ctx = canvas.getContext('2d');
+const widthInput    = document.getElementById('width');
+const heightInput   = document.getElementById('height');
 
-let TEXT_TO_RENDER = 'Ich bin verheiratet...';
+const HEADER_HEIGHT = 21.5;
+
+let CTX = null;
+let TEXT_TO_RENDER = `def main():
+    print('hello world')
+
+if __name__ == '__main__':
+    main()`;
+let WIDTH = 500;
+let HEIGHT = 250;
+widthInput.value = WIDTH;
+heightInput.value = HEIGHT;
 textArea.value = TEXT_TO_RENDER;
 
 function simpleThrottle(f, timeout) {
@@ -20,42 +32,77 @@ function simpleThrottle(f, timeout) {
 	}
 }
 
-function changeSize(width, height) {
-	console.log(width, height)
-	canvas.style.width = width + 'px';
-	canvas.style.height = height + 'px';
-	const scale = 7;
-	canvas.width = width * scale;
-	canvas.height = height ;
+function getContext(width, height) {
+	const scale         = window.devicePixelRatio;
+	canvas.width        = width * scale;
+	canvas.height       = height * scale;
+	canvas.style.width  = `${width}px`;
+	canvas.style.height = `${height}px`;
+	const ctx           = canvas.getContext('2d');
 	ctx.scale(scale, scale);
+	return ctx;
 }
 
-function draw(ctx, textData, cursorVisible = true) {
-	ctx.fillStyle = "#282828";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+function drawHeader(ctx) {
 	ctx.fillStyle = "#404040"
-	ctx.fillRect(0, 0, canvas.width, 21.5)
+	ctx.fillRect(0, 0, WIDTH, HEADER_HEIGHT)
 	ctx.fillStyle = "#f32b99"
 	ctx.beginPath();
-	ctx.arc(canvas.width - 10.5, 10.5, 5.5, 0, 2 * Math.PI);
+	ctx.arc(WIDTH - 10.5, 10.5, 5.5, 0, 2 * Math.PI);
 	ctx.fill();
 
 	ctx.fillStyle = "#2bf33c"
 	ctx.beginPath();
-	ctx.arc(canvas.width - 30.5, 10.5, 5.5, 0, 2 * Math.PI);
+	ctx.arc(WIDTH - 30.5, 10.5, 5.5, 0, 2 * Math.PI);
 	ctx.fill();
 
 	ctx.fillStyle = "#f3d22b"
 	ctx.beginPath();
-	ctx.arc(canvas.width - 50.5, 10.5, 5.5, 0, 2 * Math.PI);
+	ctx.arc(WIDTH - 50.5, 10.5, 5.5, 0, 2 * Math.PI);
 	ctx.fill();
+
+	ctx.fillStyle = "#ffffff";
+	ctx.font = "bold 13px arial";
+	const xPos = WIDTH / 2 - 30;
+	ctx.fillText("NVIM", xPos, 16);
+	ctx.fillText('[name]', xPos + 38, 14);
+}
+
+function drawLineNumbers(ctx) {
+	ctx.fillStyle = '#2c2c2c'
+	ctx.fillRect(0, HEADER_HEIGHT, 20, HEIGHT);
+	ctx.fillStyle = '#989696'
+	ctx.strokeStyle = '#989696'
+	ctx.beginPath();
+	ctx.moveTo(20, HEADER_HEIGHT);
+	ctx.lineTo(20, HEIGHT);
+	ctx.stroke();
+	ctx.font = '10px Arial';
+	const count = Math.floor(HEIGHT / 14);
+	for (let i = 1; i <= count; i++) {
+		ctx.fillText(i.toString(10), 15 - (i.toString().length * 5), (i - 1) * 14 + 40);
+	}
+}
+
+function draw(ctx, textData, cursorVisible = true) {
+	ctx.fillStyle = "#282828";
+	ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+	drawHeader(ctx);
+	drawLineNumbers(ctx);
 
 	ctx.font = '14px Arial';
 	
 	ctx.fillStyle = '#ffffff';
-	ctx.fillText(textData.text, textData.x, textData.y);
+	let nl = -1;
+	let lines = textData.text.split('\n');
+	let lc = lines.length;
+	lines.forEach((line, i) => {
+		nl++;
+		ctx.fillText(line, textData.x, textData.y + (i * 14));
+	});
 	if (cursorVisible) {
-		ctx.fillRect((textData.x + ctx.measureText(textData.text).width), textData.y - 11, 1.5, 14);
+		ctx.fillRect((textData.x + ctx.measureText(lines[lc - 1]).width), textData.y - 11 + (14 * nl), 1.5, 14);
 	}
 }
 
@@ -67,7 +114,7 @@ function *writer(text) {
 	let direct = true;
 	let countWS = 0;
 	const shouldSkip = (ch) => {
-		if (countWS < 8 && ch === ' ') {
+		if (countWS < 3 && ch === ' ') {
 			countWS++;
 			return true
 		}
@@ -105,7 +152,7 @@ function *writer(text) {
 	}
 }
 let write = writer(textArea.value);
-const getText = simpleThrottle(() => write.next().value, 30);
+const getText = simpleThrottle(() => write.next().value, 20);
 const getVisible = simpleThrottle((s) => !s, 80);
 
 let recordGif = false;
@@ -119,8 +166,8 @@ function startRecording() {
 	gif = new GIF({
 		workers: 5,
 		quality: 10,
-		width: canvas.width,
-		height: canvas.height,
+		width: WIDTH * 2,
+		height: HEIGHT * 2,
 	});
 	gif.setOption('debug', true);
 	recordGif = true;
@@ -135,6 +182,7 @@ function stopRecording() {
 	recordGif = false;
 
 	gif.on('finished', (blob) => {
+		console.log(blob);
 		const button = document.createElement('button');
 		button.textContent = 'Remove';
 		document.body.appendChild(button)
@@ -142,8 +190,8 @@ function stopRecording() {
 		const img = document.createElement('img');
 		img.src = URL.createObjectURL(blob);
 		img.style.display = "block";
-		img.style.width = canvas.width;
-		img.style.height = canvas.height;
+		img.style.width = `${WIDTH}px`;
+		img.style.height = `${HEIGHT}px`;
 		img.style.marginTop = '10px';
 		document.body.appendChild(img)
 		const a = document.createElement('a');
@@ -169,24 +217,26 @@ function stopRecording() {
 }
 
 function renderLoop() {
+	if (!CTX || WIDTH !== widthInput.value || HEIGHT !== heightInput.value) {
+		WIDTH = widthInput.value;
+		HEIGHT = heightInput.value;
+		CTX = getContext(WIDTH, HEIGHT);
+	}
 	if (textArea.value !== TEXT_TO_RENDER) {
-		// changeSize(ctx.measureText(textArea.value).width + 40, 200);
 		TEXT_TO_RENDER = textArea.value;
-		canvas.width = ctx.measureText(textArea.value).width + 40;
 		i = 0;
 		text = '';
 		write = writer(textArea.value);
 	}
 	if (recordGif) {
-		const img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		const img = CTX.getImageData(0, 0, WIDTH * 2, HEIGHT * 2);
 		gif.addFrame(img, {delay: 1.0 / 60 * 2000, dispose: -1,});
 	}
 	cursorVisible = getVisible(cursorVisible);
 	text = getText();
-	draw(ctx, {text, x: 20, y: canvas.height / 2}, cursorVisible)
+	draw(CTX, {text, x: 30, y: 40}, cursorVisible)
 	requestAnimationFrame(renderLoop);
 }
-// changeSize(ctx.measureText(textArea.value).width + 40, 200);
 renderLoop();
 
 
